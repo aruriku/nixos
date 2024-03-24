46bfa243dec6f896ebb0c5c9b8cf2c2fec3a995f
aruriku <164705500+aruriku@users.noreply.github.com>
2024-03-24 11:23:32 -0300

    move gnome vrr overlay to gnome module

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -11,27 +11,7 @@
     ];
 
   #nixos overlays
-  nixpkgs.overlays = [ (final: prev: {
-  # elements of pkgs.gnome must be taken from gfinal and gprev
-  gnome = prev.gnome.overrideScope' (gfinal: gprev: {
-    mutter = gprev.mutter.overrideAttrs (oldAttrs: {
-      patches = (oldAttrs.patches or []) ++ [
-        (prev.fetchpatch {
-          url = "https://aur.archlinux.org/cgit/aur.git/plain/vrr.patch?h=mutter-vrr";
-          sha256 = "h3Z3x/I8pvfUf3Na04y+lr1/WTbgAUVanRrLLKx3EW8=";
-        })
-    ];
-    });
-    gnome-control-center = gprev.gnome-control-center.overrideAttrs (oldAttrs: {
-      patches = (oldAttrs.patches or []) ++ [
-        (prev.fetchpatch {
-          url = "https://aur.archlinux.org/cgit/aur.git/plain/734.patch?h=gnome-control-center-vrr";
-          sha256 = "8FGPLTDWbPjY1ulVxJnWORmeCdWKvNKcv9OqOQ1k/bE=";
-        })
-      ];
-    });
-  });
-}) ];
+  
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
