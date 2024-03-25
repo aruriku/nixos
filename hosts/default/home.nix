47bc7d174a13865740bc34bd8d54164f499f7b4c
aruriku <164705500+aruriku@users.noreply.github.com>
2024-03-25 14:16:05 -0300

    moved bash shell stuff to a module

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -1,6 +1,12 @@
-{ config, pkgs, ... }:
+{ config, pkgs, outputs, ... }:
 
 {
+  imports = [
+    outputs.homeManagerModules.bash
+  
+  ];
+
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "gambit";
@@ -92,17 +98,6 @@
   #
   home.sessionVariables = {
      EDITOR = "vim";
-  };
-  # shell aliases
-  programs.bash = {
-    enable = true;
-    shellAliases = {
-     nu = "nix flake update ~/nixos/ && sudo nixos-rebuild switch --flake ~/nixos/#default";
-     nr = "sudo nixos-rebuild switch --flake ~/nixos/#default";
-     ng = "echo 'cleaning garbage & nix iterations older than 20days...';sudo nix-collect-garbage --delete-older-than 20d";
-     ls = "lsd";
-     yt-archive = "yt-dlp --embed-thumbnail --embed-subs --embed-metadata --embed-chapters --concurrent-fragments 3 --cookies-from-browser FIREFOX:3qxeajmi.default-release-1661478644995 --embed-chapters --sponsorblock-mark all --embed-info-json --sub-langs all,-live_chat --download-archive archive.txt --extractor-args 'youtube:lang=en'"; 
-    };
   };
   
 
