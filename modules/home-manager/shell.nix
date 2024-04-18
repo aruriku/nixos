4fb759355afa6d0040cd6ed9ba0d0b080d8c4288
aruriku <164705500+aruriku@users.noreply.github.com>
2024-04-18 13:17:24 -0300

    remove hardcoded #default from aliases.

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -3,10 +3,10 @@
   programs.bash = {
     enable = true;
     shellAliases = {
-      nu = "nix flake update ~/nixos/ && sudo nixos-rebuild switch --flake ~/nixos/#default";
-      nr = "sudo nixos-rebuild switch --flake ~/nixos/#default";
+      nu = "nix flake update ~/nixos/ && sudo nixos-rebuild switch --flake ~/nixos/";
+      nr = "sudo nixos-rebuild switch --flake ~/nixos/";
       ng = "echo 'cleaning garbage & nix iterations older than 20days...' && sudo nix-collect-garbage --delete-older-than 20d";
-      nd = "echo 'doing a dry-run...' && sudo nixos-rebuild dry-run --flake ~/nixos/#default";
+      nd = "echo 'doing a dry-run...' && sudo nixos-rebuild dry-run --flake ~/nixos/";
       ls = "lsd";
       la = "lsd -a";
       yt-archive = "yt-dlp --embed-thumbnail --embed-subs --embed-metadata --embed-chapters --concurrent-fragments 3 --cookies-from-browser FIREFOX:default --embed-chapters --sponsorblock-mark all --embed-info-json --sub-langs all,-live_chat --download-archive archive.txt --extractor-args 'youtube:lang=en'"; 
