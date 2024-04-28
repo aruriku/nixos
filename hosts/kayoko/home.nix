772c950f47b4a2c9da2b0d2bb60a14c8a0e15abf
aruriku <164705500+aruriku@users.noreply.github.com>
2024-04-28 03:07:29 +0000

    follow template and remove workaround

--- hosts/kayoko/home.nix
+++ hosts/kayoko/home.nix
@@ -47,8 +47,6 @@
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
     };
   };
 
