7f5625972c5f0a08fab45cfd7d2dcb09c3299bb0
aruriku <164705500+aruriku@users.noreply.github.com>
2024-06-13 21:19:08 -0300

    change wine version to unstableFull

--- hosts/haruka/configuration.nix
+++ hosts/haruka/configuration.nix
@@ -95,7 +95,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
 	vim
-	wineWowPackages.waylandFull
+	wineWowPackages.unstableFull
 	ffmpegthumbnailer
 	firefox
   ];
--- hosts/kayoko/configuration.nix
+++ hosts/kayoko/configuration.nix
@@ -94,7 +94,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
 	vim
-	wineWowPackages.waylandFull
+	wineWowPackages.unstableFull
 	ffmpegthumbnailer
 	firefox
 
