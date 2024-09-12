b62ba7a82ae9e3a5c519e6c3e23c9a7fcfb8e786
aruriku <164705500+aruriku@users.noreply.github.com>
2024-09-12 18:13:08 -0300

    Revert "Add the search engine proxy "4get" and set it as default on firefox." Very annoying captcha!
    This reverts commit 7f49cf5a0bb987729c03207ffd4b92cf1acec987.

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -9,36 +9,27 @@
         name = "default";
         isDefault = true;
         path = "default";
-        search = {
-          engines = {
-            # add nix packages as a search engine
-            "Nix Packages" = {
-              urls = [{
-                template = "https://search.nixos.org/packages";
-                params = [
-                  { name = "type"; value = "packages"; }
-                  { name = "query"; value = "{searchTerms}"; }
-                ];
-              }];
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@np" ];
-            };
-            "4get" = {
-              urls = [{ template = "https://4get.ca/web?s={searchTerms}"; }];
-              iconUpdateURL = "https://4get.ca/favicon.ico";
-              updateInterval = 7 * 24 * 60 * 60 * 1000; #update once a week
-              definedAliases = [ "@4g" ];
-            };
-            # disable bing
-            "Bing".metaData.hidden = true;
+        search.engines = {
+          # add nix packages as a search engine
+          "Nix Packages" = {
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "type"; value = "packages"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
           };
-          #force search config changes (replaces configuration)
-          force = true;
-          # set 4get to the default
-          default = "4get";
-          # prioritise google and nix packages in search
-          order = [ "4get" "Google" "Nix Packages"];
+          # disable bing
+          "Bing".metaData.hidden = true;
         };
+        #force search config changes (replaces configuration)
+        search.force = true;
+        # prioritise google and nix packages in search
+        search.order = [ "Google" "Nix Packages"];
+
         settings = {
           # Enable hardware decoding support
           "media.ffmpeg.vaapi.enabled" = true;
